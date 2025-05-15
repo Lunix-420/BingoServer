ecc471a9926f593cec882c5e6ebdf39aae51ff75
Lunix-420 <davidhess336@gmx.de>
2025-05-15 15:07:46 +0200

    feat: Add route for rooms in the main router

--- routes/Router.js
+++ routes/Router.js
@@ -5,10 +5,12 @@ const router = express.Router();
 const echoRoute = require("./Echo.js");
 const tilesetsRoute = require("./Tilesets.js");
 const bingofieldsRoute = require("./Bingofields.js");
+const roomRoute = require("./Room.js");
 
 // Mount the echo route as a subroute
 router.use("/echo", echoRoute);
 router.use("/tilesets", tilesetsRoute);
 router.use("/bingofields", bingofieldsRoute);
+router.use("/rooms", roomRoute);
 
 module.exports = router;
