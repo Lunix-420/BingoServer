fd1f2fb21181d25d58dfe8e103de315134e23d97
Lunix-420 <davidhess336@gmx.de>
2025-06-10 15:34:22 +0200

    feat: Extend Room schema to include additional fields for game management

--- models/Room.js
+++ models/Room.js
@@ -18,7 +18,49 @@ const RoomSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    tileset: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Tileset",
+      required: true,
+    },
+    players: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Player",
+      },
+    ],
+    bingofields: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Bingofield",
+      },
+    ],
+    host: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Player",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["waiting", "started", "finished"],
+      default: "waiting",
+    },
+    maxPlayers: {
+      type: Number,
+      default: 4,
+      min: 2,
+      max: 10,
+    },
+    startedAt: {
+      type: Date,
+      default: null,
+    },
+    endedAt: {
+      type: Date,
+      default: null,
+    },
   },
+
   { timestamps: true }
 );
 
