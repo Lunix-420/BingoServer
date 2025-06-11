6bbd311669d4e50c1f221038a4a2e3b44e8646c1
Lunix-420 <davidhess336@gmx.de>
2025-06-11 18:40:43 +0200

    fix: Correct roomHas variable name to roomHash in updateGameState event handler

--- sockets/Server.js
+++ sockets/Server.js
@@ -27,7 +27,7 @@ function initializeSocket(server) {
     });
 
     // Broadcast game state update to the room
-    socket.on("updateGameState", ({ roomHas }) => {
+    socket.on("updateGameState", ({ roomHash }) => {
       console.log(`Game state update from ${socket.id} to room ${roomHash}`);
       socket.to(roomHash).emit("gameStateUpdated");
     });
